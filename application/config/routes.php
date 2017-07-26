8e96b8349e08c1144f98208d124c3173d25cbb2a
Brian Hume <brian.hume@synapse.com>
2017-07-26 15:07:53 -0300

    Added post view

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //$route['default_controller'] = 'welcome';
+$route['posts/(:any)'] = 'posts/view/$1';
 $route['posts'] = 'posts/index';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,18 +1,20 @@
 <html>
     <head>
       <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
       <title>CodeIgniter App</title>
     </head>
     <body>
       <nav class="navbar navbar-inverse">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="/">ciApp</a>
+            <a class="navbar-brand" href="<?php echo base_url(); ?>">ciApp</a>
           </div>
           <div id="navbar">
             <ul class="nav navbar-nav">
               <li><a href="<?php echo base_url(); ?>">Home</a></li>
-              <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+              <li><a href="<?php echo base_url(); ?>posts">Posts</a></li>
+              <li><a href="<?php echo base_url(); ?>about">About</a></li>
             </ul>
           </div>
         </div>
