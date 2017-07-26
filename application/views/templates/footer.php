54a6e070b09e4bce02d76f8942c05688608fdebd
Brian Hume <brian.hume@synapse.com>
2017-07-26 13:08:11 -0300

    Start CodeIgniter project

---
+++ application/views/templates/footer.php
@@ -0,0 +1,4 @@
+    </div>
+    <em>&copy; 2017</em>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+      <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+      <title>CodeIgniter App</title>
+    </head>
+    <body>
+      <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">ciApp</a>
+          </div>
+          <div id="navbar">
+            <ul class="nav navbar-nav">
+              <li><a href="<?php echo base_url(); ?>">Home</a></li>
+              <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container">
